5502b47e8d376a548585229973f0fa83bfd5dc13
mahmoudeltaweel <mahmoudeltaweel888@gmail.com>
2024-07-29 17:51:02 +0300

    add categories

--- src/Dashboard/Categories.js
+++ src/Dashboard/Categories.js
@@ -1,29 +1,26 @@
-import { useEffect } from "react"
-// import { Link } from "react-router-dom";
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
 
 export default function Categories(){
 
-    // const[data , setdata]=useState([]);
-    // const[rundelte , setrundelte]=useState([]);
+    const[data , setdata]=useState([]);
 
 
 
     useEffect(()=>{
-        fetch("http://localhost:9000/products/category")
+        fetch("http://localhost:9000/categories")
         .then((res)=>res.json())
-        .then((data)=>console.log(data))
+        .then((data)=>setdata(data))
     },[])
 
-    // let showdata = data.map((item ,index)=>
-    //     <>
-    //     <tr  key={index}>
-    //   <td>{index+1}</td>
-    //   <td>{item.title}</td>
-    //   <td>{item.category}</td>
-    //   <td>{item.price}</td>
-    // </tr>
-    // </>
-    // )
+    let showdata = data.map((item ,index)=>
+        <>
+        <tr  key={index}>
+      <td style={{textAlign:"center"}}>{index+1}</td>
+      <td style={{textAlign:"center"}}>{item}</td>
+    </tr>
+    </>
+    )
 
     return(
         <>
@@ -31,14 +28,11 @@ export default function Categories(){
   <thead>
     <tr>
       <th scope="col" style={{textAlign:"center"}}>Num</th>
-      <th scope="col" style={{textAlign:"center"}}>title</th>
       <th scope="col" style={{textAlign:"center"}}>category</th>
-      <th scope="col" style={{textAlign:"center"}}>price</th>
-      <th  style={{textAlign:"center" , width:"100px"}}>Action</th>
     </tr>
   </thead>
   <tbody>
-    {/* {showdata} */}
+    {showdata}
   </tbody>
 </table>
 </>
